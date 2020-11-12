e72f20db3118be93679f9199fafee5f7321fa18b
jonn <jonn@jonn.nu>
2020-11-12 19:14:52 +0100

    test: add unit test for in memory client

---
+++ src/class/RedisInMemoryClient.test.ts
@@ -0,0 +1,39 @@
+import { RedisInMemoryClient } from "./RedisInMemoryClient";
+
+describe("RedisInMemoryClient", () => {
+  let client: RedisInMemoryClient;
+
+  beforeEach(async () => {
+    client = new RedisInMemoryClient({ port: 1 });
+    await client.connect();
+  });
+
+  test("should quit", async () => {
+    await expect(client.quit()).resolves.toBe("OK");
+  });
+
+  test("should set", async () => {
+    await expect(client.set("key", { blob: { object: { is: true } } })).resolves.toBe("OK");
+    expect(client.store).toMatchSnapshot();
+  });
+
+  test("should get", async () => {
+    await expect(client.set("key", { blob: "yes" })).resolves.toBe("OK");
+
+    await expect(client.get("key")).resolves.toMatchSnapshot();
+  });
+
+  test("should get all", async () => {
+    await expect(client.set("key1", { blob: "yes" })).resolves.toBe("OK");
+    await expect(client.set("key2", { blob: "no" })).resolves.toBe("OK");
+
+    await expect(client.getAll("key*")).resolves.toMatchSnapshot();
+  });
+
+  test("should delete", async () => {
+    await expect(client.set("key", { blob: "yes" })).resolves.toBe("OK");
+
+    await expect(client.del("key")).resolves.toBe(1);
+    expect(client.store).toMatchSnapshot();
+  });
+});
