76a96014c76a06a14ad5dbbb8f155a72bb9e14eb
jonn <jonn@jonn.nu>
2020-11-30 10:24:23 +0100

    fix: export errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./cache";
 export * from "./class";
 export * from "./enum";
+export * from "./error";
 export * from "./infrastructure";
 export * from "./typing";
 export * from "./util";
